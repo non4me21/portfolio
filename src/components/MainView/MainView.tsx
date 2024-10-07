bc983c09fc1a7844971676ea1e401c5c69683721
non4me21 <kacper.w21@wp.pl>
2024-10-07 10:46:51 +0200

    new test

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -4,6 +4,7 @@ import { ProjectsGrid } from '../ProjectsGrid/ProjectsGrid'
 import { SideInfo } from '../SideInfo/SideInfo'
 import styles from './MainView.module.scss'
 import projectsContent from '../../content/projects.json'
+import vite from '../../../public/vite.svg'
 
 export const MainView = () => {
 
@@ -19,7 +20,7 @@ export const MainView = () => {
         {activeTab === 0 && <Overview />}
         {activeTab === 1 && <ProjectsGrid projects={projectsContent.ProjectsGrid.projects ?? []}/>}
       </div>
-      <img src='public/vite.svg' />
+      <img src={vite} />
     </div>
   )
 }
